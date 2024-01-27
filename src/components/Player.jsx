e0116391f7b98bf7142b3f27460cf3be0200e89a
Nikhil Mudgal <nikhilm2097@gmail.com>
2024-01-28 00:32:04 +0530

    Made input fields editable

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName)
     const [ isEditing, setIsEditing ]  = useState(false);
 
     function editPlayerName() {
-        setIsEditing(!isEditing);
+        // setIsEditing(!isEditing); // never change state like this. this is not a best practice because state updats are not performed instantly but at some point in the future whenever react has time for it
+        setIsEditing((editing) => !editing); // this function will be automatically called by react and will receive the guaranteed latest state value
     }
 
-    let playerName = <span className="player-name">{name}</span>
+    function handleChange(event) { // onchange will pass the $event parameter in handleChange() whenever onChange is triggered
+        console.log(event)
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
     if(isEditing) {
-        playerName = <input type="text" required />
+        editablePlayerName = <input type="text" required value = {playerName} onChange={handleChange} />
     }
     return (
         <li>
             <span className="player">
-            { playerName }
+            { editablePlayerName }
             <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={ editPlayerName}>Edit</button>
+            <button onClick={ editPlayerName}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     )
 }
