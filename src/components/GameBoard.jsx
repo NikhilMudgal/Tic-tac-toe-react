f8d43dafa52650d744afba58323d4ce06290eff9
Nikhil Mudgal <nikhilm2097@gmail.com>
2024-02-02 00:37:59 +0530

    Added win condition

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,32 +1,7 @@
-// import { useState } from "react";
-
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
-
-export default function GameBoard ({ onSelectSquare, turns }) {
-    let gameBoard = initialGameBoard;
-    for(const turn of turns) {
-        const {square, player } = turn;
-        const { row, col } = square;
-        gameBoard[row][col] = player;
-    }
-
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    // function handleSelectSquare(rowIndex, colIndex) {
-    //     setGameBoard((prevGameBoard) => {
-    //         const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-    //         updateBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //         return updateBoard;
-    //     });
-    //     onSelectSquare(); // active player will be changed after the previous has filled the box
-    // }
+export default function GameBoard ({ onSelectSquare, board }) {
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <li key = {rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
